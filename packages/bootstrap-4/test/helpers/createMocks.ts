cbe79b0754df3834cd35308f99bd4ca033d6d5d7
Anix <anik220798@gmail.com>
2020-06-23 10:33:13 +0000

    chore: upate the tests and added mock handlers

---
+++ packages/bootstrap-4/test/helpers/createMocks.ts
@@ -0,0 +1,33 @@
+import { WidgetProps } from "@rjsf/core";
+import { JSONSchema7 } from "json-schema";
+
+export const mockSchema: JSONSchema7 = {
+  type: "array",
+  items: {
+    type: "string",
+  },
+};
+
+export const mockEventHandlers = (): void => void 0;
+
+export function makeWidgetMockProps(props: Partial<WidgetProps>): WidgetProps {
+  return {
+    type: "color",
+    schema: mockSchema,
+    required: true,
+    disabled: false,
+    readonly: true,
+    autofocus: true,
+    label: "Some simple label",
+    onChange: mockEventHandlers,
+    onBlur: mockEventHandlers,
+    onFocus: mockEventHandlers,
+    multiple: false,
+    rawErrors: [""],
+    value: "value",
+    options: {},
+    formContext: {},
+    id: "_id",
+    ...props,
+  };
+}
